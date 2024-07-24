2e0c94ce46a404e12c7519e33fd026fd427dd15f
arata-7 <arata.tanaka8@gmail.com>
2024-07-24 14:26:57 +0200

    add pagination to id.js

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,8 +1,11 @@
 // pages/blog/[id].js
 import { client } from "../../libs/client";
-import styles from "../../styles/Home.module.scss"
+import styles from "../../styles/Home.module.scss";
+import { Pagination } from "../../components/Pagination";
 
-export default function BlogId({ blog }) {
+const PER_PAGE = 5;
+
+export default function BlogId({ blog, totalCount }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
@@ -14,26 +17,32 @@ export default function BlogId({ blog }) {
           __html: `${blog.body}`,
         }}
       />
+      <Pagination totalCount={totalCount} />
     </main>
   );
 }
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
-  const data = await client.get({ endpoint: "blog" });
-
-  const paths = data.contents.map((content) => `/blog/${content.id}`);
-  return { paths, fallback: false };
-};
+    const repos = await client.get({ endpoint: "blog" });
+  
+    const range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
+  
+    const paths = range(1, Math.ceil(repos.totalCount / PER_PAGE)).map((repo) => `/blog/page/${repo}`);
+  
+    return { paths, fallback: false };
+  };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const data = await client.get({ endpoint: "blog", contentId: id });
-
-  return {
-    props: {
-      blog: data,
-    },
+    const id = context.params.id;
+  
+    const data = await client.get({ endpoint: "blog", queries: { offset: (id - 1) * PER_PAGE, limit: PER_PAGE } });
+  
+    return {
+      props: {
+        blog: data.contents,
+        totalCount: data.totalCount,
+      },
+    };
   };
-};
